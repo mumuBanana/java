27fe347b7e35ae74a0d649ab575858586b7e8dfd
mumu <2429958274@qq.com>
2019-04-07 20:58:51 +0800

    第二天学习

---
+++ java/day_02/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+	public static void main(String[] args){
+		byte a = 3;
+		short b = 4000;
+		int c = 12;
+		long d = 12354l;
+		float e = 2.3f;
+		
+		double f = 3.4;
+		
+		char g = 'a';
+		
+		boolean h = true;
+		h = false;
+		
+		//System.out.println(h);
+		//System.out.println('你'+0); //unicode 国际标准码表
+	}
+}
